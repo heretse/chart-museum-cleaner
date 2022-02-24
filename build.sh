4bb65c2ace548f29672655fd3b1d907f95af0172
Winston Hsieh <heretse@gmail.com>
2022-02-24 23:39:30 +0800

    Support USER env for basic authentication

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t heretse/chartmuseum-cleaner:1.0.0 .
--- chart_museum_cleaner/api.py
+++ chart_museum_cleaner/api.py
@@ -2,8 +2,11 @@ import os
 
 import requests
 
+import base64
+
 BASE_URL = os.getenv("CHART_MUSEUM_URL", "http://localhost:8080")
 TOKEN = os.getenv('TOKEN', '')
+USER = os.getenv('USER', '')
 
 api = 'api'
 
@@ -11,13 +14,22 @@ charts = 'charts'
 
 
 def list_all_charts():
-    return requests.get(f'{BASE_URL}/{api}/{charts}').json()
+    if USER:
+        return requests.get(f'{BASE_URL}/{api}/{charts}',
+                            headers={'Authorization': f'Basic {base64.b64encode(USER.encode("UTF-8")).decode("UTF-8")}'}
+                            ).json()
+    else:
+        return requests.get(f'{BASE_URL}/{api}/{charts}').json()
 
 
 def delete_chart_version(name, version):
     if TOKEN:
         return requests.delete(f'{BASE_URL}/{api}/{charts}/{name}/{version}',
-                               header={'Authorization': f'Bearer {TOKEN}'}
+                               headers={'Authorization': f'Bearer {TOKEN}'}
+                               )
+    elif USER:
+        return requests.delete(f'{BASE_URL}/{api}/{charts}/{name}/{version}',
+                               headers={'Authorization': f'Basic {base64.b64encode(USER.encode("UTF-8")).decode("UTF-8")}'}
                                )
     else:
         return requests.delete(f'{BASE_URL}/{api}/{charts}/{name}/{version}')
